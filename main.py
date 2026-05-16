7eb15c3871bd25deddbb5856eaff3ad76d5edf90
sobchaqdimka-sudo <sobchaqdimka@gmail.com>
2026-05-16 17:11:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,12 +45,11 @@ def process_video():
         result = subprocess.run(cmd, capture_output=True, text=True)
 
         if result.returncode != 0:
-    return jsonify({
-        "error": "ffmpeg failed",
-        "cmd": " ".join(cmd),
-        "details": result.stderr[-4000:]
-    }), 500
-
+            return jsonify({
+                "error": "ffmpeg failed",
+                "cmd": " ".join(cmd),
+                "details": result.stderr[-4000:]
+            }), 500
         return send_file(output_path, mimetype="video/mp4", as_attachment=True, download_name="processed.mp4")
 
     except Exception as e:
